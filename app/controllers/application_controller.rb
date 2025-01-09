ea335579378cd93279f09a1529fc3e615ab2cb90
ravilgaifullin <ravil.gaifullin@nixsolutions.com>
2025-01-09 21:03:00 +0100

    adds 404 processing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: {:error => exception.message}, status: 404
+  end
 end
+
